32e26529028f24be55753d41a22188a452bef7a0
Ayush <ayushsingh12march@gmail.com>
2020-04-22 12:08:40 +0530

    Added fields to orders,order_items table

--- db/migrate/20200422061819_create_orders.rb
+++ db/migrate/20200422061819_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.date
+      t.date :date
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/migrate/20200422061919_create_order_items.rb
+++ db/migrate/20200422061919_create_order_items.rb
@@ -1,7 +1,10 @@
 class CreateOrderItems < ActiveRecord::Migration[6.0]
   def change
     create_table :order_items do |t|
-
+      t.integer :order_id
+      t.integer :menu_item_id
+      t.string :menu_item_name
+      t.float :menu_item_price
       t.timestamps
     end
   end
