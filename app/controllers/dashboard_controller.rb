cbcd4fd29aa01567995280cfd0e9e6843bdf9a3c
Ayush <ayushsingh12march@gmail.com>
2020-05-05 08:47:10 +0530

    FEAT : link for menuwise chart added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,11 +10,13 @@ class DashboardController < ApplicationController
     # dish_menu.each do |dish|
     #   @dish_count[menu_hash[dish.first]] = dish.second if menu_hash[dish.first]
     # end
-    results_with_count = ActiveRecord::Base.connection.execute("select menu_items.name,count(*) FROM menu_items INNER JOIN order_items ON menu_items.id=order_items.menu_item_id WHERE menu_items.menu_id=1  GROUP BY menu_items.id")  ##{pass params from erb for menu_id}
+    @menu = Menu.all.pluck_to_hash(:id)
+    @active_menu = Menu.where("active=?", true).pluck_to_hash(:name)[0]
+    results_with_count = ActiveRecord::Base.connection.execute("select menu_items.name,count(*) FROM menu_items INNER JOIN order_items ON menu_items.id=order_items.menu_item_id WHERE menu_items.menu_id=#{params[:id] || 1}  GROUP BY menu_items.id")  ##{pass params from erb for menu_id}
     @dish_and_count = {}
     results_with_count.each { |res| @dish_and_count[res["name"]] = res["count"] }
 
-    results_with_price = ActiveRecord::Base.connection.execute("select menu_items.name,count(*),menu_items.price FROM menu_items INNER JOIN order_items ON menu_items.id=order_items.menu_item_id WHERE menu_items.menu_id=1  GROUP BY menu_items.id")
+    results_with_price = ActiveRecord::Base.connection.execute("select menu_items.name,count(*),menu_items.price FROM menu_items INNER JOIN order_items ON menu_items.id=order_items.menu_item_id WHERE menu_items.menu_id=#{params[:id] || 1}  GROUP BY menu_items.id")
     # {pass params from erb for menu_id}
     @dish_and_amount = {}
     results_with_price.each { |res| @dish_and_amount[res["name"]] = (res["count"] * res["price"]) }
@@ -22,5 +24,10 @@ class DashboardController < ApplicationController
     revenue = ActiveRecord::Base.connection.execute("select date(updated_at),total FROM orders WHERE status ='Completed' ")
     @revenue_daily = {}
     revenue.each { |res| @revenue_daily[res["date"]] = res["total"] }
+
+    @completed_and_pending = Order.group(:status).count
+  end
+
+  def menu_dash
   end
 end
